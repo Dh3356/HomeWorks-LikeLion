f34b1a48864304e3dfe3da46a6581abc9ed489ad
KimDaeHyeon <daehyeon3351@gmail.com>
2023-04-07 21:02:31 +0900

    solved slide

--- homework2/slide/api/randomImage.js
+++ homework2/slide/api/randomImage.js
@@ -3,4 +3,16 @@ import getURL from './URL.js';
 const getRandomImage = async () => {
     const randomAlbumId = Math.floor(Math.random() * (10 - 1) + 1);//Math.floor() 함수는 주어진 숫자와 같거나 작은 정수 중에서 가장 큰 수를 반환한다.
     const BASE_URL = getURL(randomAlbumId);
-}
+
+    try{
+        const result = await fetch(BASE_URL);//fetch()가 끝날 때 까지 기다렸다가 다음 코드 실행
+        const content = await result.json();//result()가 끝날 때 까지 기다렸다가 다음 코드 실행, 이미지 객체를 10개
+
+        return content;
+    }
+    catch (e){
+        console.log('api 에러!', e.message);
+    }
+};
+
+export default getRandomImage;
--- homework2/slide/index.js
+++ homework2/slide/index.js
@@ -1,1 +1,103 @@
 // 여기에 Carousel을 조작할 코드를 작성합니다.
+import getRandomImage from './api/randomImage.js';
+
+const randomImageBtn = document.querySelector("#getImageButton");
+const container = document.querySelector("#imageContainer");
+const counter = document.querySelector("#imageCounter");
+const leftBtn = document.querySelector("#moveLeft");
+const rightBtn = document.querySelector("#moveRight");
+
+window.addEventListener('DOMContentLoaded', () => {
+    registButtonEvent();
+});//
+
+
+let totalCount = 0;//총 페이지 번호
+let currentCount = 0;//현재 페이지 번호
+
+const registButtonEvent = () => {//DOMContentLoaded 됐을 때 실행하는 함수
+    randomImageBtn.addEventListener('click', async () => {//randomImage Button click 시 발생하는 이벤트
+        const imageData = await getRandomImage();//Random Image를 받아온다
+        const slicedImageData = imageData.slice(0, 10);//10 개만
+
+        container.innerHTML = '';//container의 내용을 비운다
+        totalCount = 0;//총 페이지 번호를 0으로 초기화한다
+        currentCount = 0;//현재 페이지 번호를 0으로 초기화한다
+        container.scrollTo({//container를 0으로 smooth하게 scroll한다
+            left: 0,
+            behavior: 'smooth',
+        });
+        console.log(slicedImageData);
+        slicedImageData.forEach((data) => renderImage(data));//sliceImageData에 있는 모든 이미지 객체들을 renderImage() 한다
+        totalCount = slicedImageData.length;//totalCount(총 페이지 번호)를 sliceImageData의 길이로 만든다
+        renderCounter(0);//counter를 rendering한다
+    });
+
+    leftBtn.addEventListener('click', () => moveImage('left'));//leftBtn을 눌렀을 때 moveImage('left')가 실행되게 한다
+    rightBtn.addEventListener('click', () => moveImage('right'));//rightBtn을 눌렀을 때 moveImage('right')가 실행되게 한다
+};
+
+
+/** @param {{url: string, id: string, title: string}} data */
+
+const renderImage = (data) => {//이미지를 rendering한다
+    const divEl = document.createElement('div');//div 태그를 만든다
+    const imgEl = document.createElement('img');//img 태그를 만든다
+    const pEl = document.createElement('p');//p 태그를 만든다
+
+    imgEl.src = data.url;//이미지의 경로(src)에 data의 url을 넣는다
+    imgEl.alt = '슬라이드이미지';//이미지가 로드되지 않으면 '슬라이드이미지'를 대신 보여 준다
+
+    pEl.textContent = data.title;//p 태그의 text 내용을 data.title로 만든다
+
+    divEl.append(imgEl);//div태그에 img와 p태그를 넣는다
+    divEl.append(pEl);
+    container.append(divEl);//container에 div태그를 붙인다
+};
+
+
+/**
+ * @param currentCount
+ */
+const renderCounter = (currentCount) => {
+    counter.textContent = `${currentCount + 1} / ${totalCount}`;//counter의 내용을 ${currentCount + 1} / ${totalCount}로 만든다
+};
+
+
+/**
+ * Carousel을 움직입니다.
+ * @param {'left' | 'right'} type
+ */
+const moveImage = (type) => {//이미지를 움직인다
+    if(type === 'left'){
+        moveLeft();
+    }
+    if(type === 'right'){
+        moveRight();
+    }
+};
+
+const moveLeft = () => {//이미지를 왼쪽으로 움직인다
+    if(currentCount > 0){//만약 currentCount가 0 보다 크다면
+        container.scrollTo({left: --currentCount * 600, behavior: 'smooth'});//currentCount를 하나 감소시키고, 감소된 CurrnetCount*600 위치로 scroll한다
+    }
+    else{//만약 currentCount가 0 보다 작거나 같다면
+        container.scrollTo({left: totalCount * 600, behavior: 'smooth'});//totalCount*600 위치로 스크롤한다
+        currentCount = totalCount - 1;//currentCount를 totalCount-1로 만든다
+    }
+    renderCounter(currentCount);//Counter를 rendering한다
+};
+
+const moveRight = () => {//이미지를 오른쪽으로 움직인다
+    if(currentCount < totalCount - 1){//만약 currentCount가 totalCount - 1보다 작다면
+        container.scrollTo({
+            left: ++currentCount * 600,
+            behavior: 'smooth',
+        });//currentCount를 하나 증가시키고 currentCount * 600 위치로 scroll한다
+    }
+    else{//만약 currentCount가 totalCount - 1보다 크거나 같다면
+        container.scrollTo({left: 0, behavior:'smooth'});//0 위치로 scroll한다
+        currentCount = 0;
+    }
+    renderCounter(currentCount);//Counter를 rendering한다
+};
